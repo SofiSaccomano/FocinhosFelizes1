6afc1915c23d2d5261455507df753a04278d7d1b
kawanydb <kawanydomingosbatista15@gmail.com>
2025-04-24 12:10:26 -0300

    alteração dos edits

---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace FocinhosFelizes1.Models
+{
+    public class Usuario
+    {
+        public Guid UsuarioId { get; set; }
+        public string NomeCompleto { get; set; }
+
+        public string Celular { get; set; }
+        public string CPF { get; set; }
+        public Guid? AppUserId { get; set; }
+    }
+}
