43c44a05df467f110b8cb3c3a3970009f7203c4d
SofiSaccomano <sofisaccomano@gmail.com>
2025-09-26 12:19:18 -0300

    rafael arrumando cagadas

---
+++ Migrations/20250926150630_DbForLocalProject.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FocinhosFelizes1.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbForLocalProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
